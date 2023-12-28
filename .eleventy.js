1db68f5a9b1737ab0571daf5783b2a814eb6211e
LAPTOP-8BIKVGGP\langl <langladehugojo@gmail.com>
2023-12-28 17:23:15 +0100

    update test config.yml

--- .eleventy.js
+++ .eleventy.js
@@ -1,48 +1,50 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("./src/css/");
-    eleventyConfig.addPassthroughCopy("./src/js/");
-    eleventyConfig.addPassthroughCopy("./src/assets/");
-    eleventyConfig.addPassthroughCopy("./src/works/");
-    eleventyConfig.addPassthroughCopy("./src/works/*.md");
-    eleventyConfig.addPassthroughCopy("./src/cv/");
-    eleventyConfig.addPassthroughCopy("./src/cv/*.md");
-    eleventyConfig.addPassthroughCopy("./src/bio/");
-    eleventyConfig.addPassthroughCopy("./src/admin/");
+  eleventyConfig.addPassthroughCopy("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/js/");
+  eleventyConfig.addPassthroughCopy("./src/assets/");
+  eleventyConfig.addPassthroughCopy("./src/works/");
+  eleventyConfig.addPassthroughCopy("./src/works/*.md");
+  eleventyConfig.addPassthroughCopy("./src/cv/");
+  eleventyConfig.addPassthroughCopy("./src/cv/*.md");
+  eleventyConfig.addPassthroughCopy("./src/bio/");
+  eleventyConfig.addPassthroughCopy("./src/images/");
 
-    eleventyConfig.setBrowserSyncConfig({
-      // Set the base directory for Eleventy to serve files from
-      server: 'dist',
-    });
-  
-    // Set the base URL for your project
-    eleventyConfig.setTemplateFormats(['njk', 'md', 'html']);
-    eleventyConfig.setDataDeepMerge(true);
+  // Corrected line: use eleventyConfig instead of config
+  eleventyConfig.addPassthroughCopy('./app/content/admin');
 
-    eleventyConfig.addCollection('works', function (collection) {
-      return collection.getFilteredByTag('works');
-    });
+  eleventyConfig.setBrowserSyncConfig({
+    // Set the base directory for Eleventy to serve files from
+    server: 'dist',
+  });
 
-    eleventyConfig.addCollection('cv', function (collection) {
-      // Use either tag-based filtering or glob-based filtering, not both
-      return collection.getFilteredByTag('cv');
-    });
+  // Set the base URL for your project
+  eleventyConfig.setTemplateFormats(['njk', 'md', 'html']);
+  eleventyConfig.setDataDeepMerge(true);
 
-    eleventyConfig.addCollection('bio', function (collection) {
-      return collection.getFilteredByTag('bio');
-    });
+  eleventyConfig.addCollection('works', function (collection) {
+    return collection.getFilteredByTag('works');
+  });
 
-    eleventyConfig.addCollection('contact', function (collection) {
-      return collection.getFilteredByTag('contact');
-    });
+  eleventyConfig.addCollection('cv', function (collection) {
+    // Use either tag-based filtering or glob-based filtering, not both
+    return collection.getFilteredByTag('cv');
+  });
 
-  
-    // Return your Object options:
-    return {
-      dir: {
-        input: "src",
-        output: "public",
-        includes: "_includes"
-      }
+  eleventyConfig.addCollection('bio', function (collection) {
+    return collection.getFilteredByTag('bio');
+  });
+
+  eleventyConfig.addCollection('contact', function (collection) {
+    return collection.getFilteredByTag('contact');
+  });
+
+  // Return your Object options:
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes"
     }
-  };
+  }
+};
